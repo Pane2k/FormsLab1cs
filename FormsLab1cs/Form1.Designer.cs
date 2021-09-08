7a905e13ff0bef3611a30177f0c807351510457c
Pane2k <82178883+Pane2k@users.noreply.github.com>
2021-09-08 10:44:37 +0300

    Добавьте файлы проекта.

---
+++ FormsLab1cs/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿
+namespace FormsLab1cs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.BoxOfX = new System.Windows.Forms.TextBox();
+            this.BoxOfY = new System.Windows.Forms.TextBox();
+            this.BoxOfZ = new System.Windows.Forms.TextBox();
+            this.ResultBox = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Execute = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // BoxOfX
+            // 
+            resources.ApplyResources(this.BoxOfX, "BoxOfX");
+            this.BoxOfX.Name = "BoxOfX";
+            this.BoxOfX.TextChanged += new System.EventHandler(this.BoxOfX_TextChanged);
+            this.BoxOfX.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.PressX);
+            // 
+            // BoxOfY
+            // 
+            resources.ApplyResources(this.BoxOfY, "BoxOfY");
+            this.BoxOfY.Name = "BoxOfY";
+            this.BoxOfY.TextChanged += new System.EventHandler(this.BoxOfY_TextChanged);
+            this.BoxOfY.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.PressY);
+            // 
+            // BoxOfZ
+            // 
+            resources.ApplyResources(this.BoxOfZ, "BoxOfZ");
+            this.BoxOfZ.Name = "BoxOfZ";
+            this.BoxOfZ.TextChanged += new System.EventHandler(this.BoxOfZ_TextChanged);
+            this.BoxOfZ.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.PressZ);
+            // 
+            // ResultBox
+            // 
+            resources.ApplyResources(this.ResultBox, "ResultBox");
+            this.ResultBox.BackColor = System.Drawing.SystemColors.Control;
+            this.ResultBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ResultBox.Name = "ResultBox";
+            this.ResultBox.ReadOnly = true;
+            this.ResultBox.TextChanged += new System.EventHandler(this.ResultBox_TextChanged);
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // Execute
+            // 
+            resources.ApplyResources(this.Execute, "Execute");
+            this.Execute.Name = "Execute";
+            this.Execute.UseVisualStyleBackColor = true;
+            this.Execute.Click += new System.EventHandler(this.Execute_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.Controls.Add(this.Execute);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.ResultBox);
+            this.Controls.Add(this.BoxOfZ);
+            this.Controls.Add(this.BoxOfY);
+            this.Controls.Add(this.BoxOfX);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox BoxOfX;
+        private System.Windows.Forms.TextBox BoxOfY;
+        private System.Windows.Forms.TextBox BoxOfZ;
+        private System.Windows.Forms.RichTextBox ResultBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button Execute;
+    }
+}
+
---
+++ FormsLab1cs/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace FormsLab1cs
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+   
+        string xString, yString, zString;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            BoxOfX.Text = "3,4";
+            BoxOfY.Text = "0,74";
+            BoxOfZ.Text = "19,43";
+        }
+
+        
+
+        private string Execution(string xString, string yString, string zString)
+        {
+            try
+            {
+                double xDouble, yDouble, zDouble, fResultDouble;
+                xDouble = Convert.ToDouble(xString);
+                yDouble = Convert.ToDouble(yString);
+                zDouble = Convert.ToDouble(zString);
+                fResultDouble = Math.Pow((yDouble + Math.Pow((xDouble - 1), 1 / 3)), 1 / 4) / (Math.Abs(xDouble - yDouble) * (Math.Pow(Math.Sin(zDouble), 2) + Math.Tan(zDouble)));
+
+                string fResultString = Convert.ToString(fResultDouble);
+                return fResultString;
+            }
+            catch
+            {
+                MessageBox.Show("Перепроверьте введенные данные", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return "";
+            }
+            finally
+            {
+            }
+        }
+
+        //блок блокировки ввода символов не являющихся цифрами
+       
+        private void PressX(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && e.KeyChar != Convert.ToChar(8) && e.KeyChar != Convert.ToChar(44))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void PressY(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && e.KeyChar != Convert.ToChar(8) && e.KeyChar != Convert.ToChar(44))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void PressZ(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && e.KeyChar != Convert.ToChar(8) && e.KeyChar != Convert.ToChar(44))
+            {
+                e.Handled = true;
+            }
+        }
+        
+        //конец блока
+
+        private void BoxOfX_TextChanged(object sender, EventArgs e)
+        {
+            xString = BoxOfX.Text;
+        }
+
+        private void BoxOfY_TextChanged(object sender, EventArgs e)
+        {
+            yString = BoxOfY.Text;
+        }
+
+        private void BoxOfZ_TextChanged(object sender, EventArgs e)
+        {
+            zString = BoxOfZ.Text;
+        }
+
+        private void ResultBox_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Execute_Click(object sender, EventArgs e)
+        {
+            ResultBox.ResetText();
+            ResultBox.Text = "Лаб. раб №1 10702220 Невыглас П.Ю.\n" + Execution(xString, yString, zString);
+        }
+    }
+}
